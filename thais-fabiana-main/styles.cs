967897bac7b68fdebef6ed205de526c696743360
thaisfabiana <128701493+thaisfabiana@users.noreply.github.com>
2024-03-07 18:08:59 -0300

    Add files via upload

--- thais-fabiana-main/styles.cs
+++ thais-fabiana-main/styles.cs
@@ -24,7 +24,7 @@ body {
 .titulo-principal { 
     text-align: left;
     width: 100%;
-    font-size: 32 px;
+    font-size: 32px;
 }
 .titulo-principal span {
 color: var (--verde);
@@ -34,17 +34,25 @@ color: var (--verde);
     background-color: var(--botao-inativo);
     color: var(--branco); display: flex;
     justify-content: center;
-    padding: 1 em;
-    font-size: 18 px;
+    padding: 1em;
+    font-size: 18px;
     align-items: center;
     width: 100%;
 }
-border-bottom: 4 px solid var(--botao-ativo);
-border-left: 2 px solid var(--botao-ativo); 
-border-right: 2 px solid var(--botao-ativo);
+border-bottom: 4px solid var(--botao-ativo);
+border-left: 2px solid var(--botao-ativo); 
+border-right: 2px solid var(--botao-ativo);
 border-top: none;
-.botoes { display: block;
+
+.botoes { 
+    display: block;
 }
 @media screen and (min-width: 768px) { .botoes { display: flex;
+}
+.botao:first-child { 
+    border-radius: 40px 0 0 0;
+}
+.botao:last-child { 
+    border-radius: 0 40px 0 0;
 }
 }
